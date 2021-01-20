725ec73d0f66995390c383f8464e17f8a7d9d728
Missael-Peralta <60717142+Missael-Peralta@users.noreply.github.com>
2021-01-19 18:01:20 -0600

    Add files via upload

---
+++ models/cliente.js
@@ -0,0 +1,13 @@
+class clientes{
+    constructor(id,nombre,apellido,domicilio,ciudad,estado,correo){
+        this.id=id;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.domicilio=domicilio; 
+        this.ciudad=ciudad;
+        this.estado=estado;
+        thiscorreo=correo;
+    }
+}
+
+module.exports=clientes;
